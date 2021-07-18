2af6bcb88a87ee1df8413d1a33f19dd6f7fc03c5
JoaquinWeiss <joaco.w93@gmail.com>
2021-07-18 18:29:13 -0300

    working trabajos section

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,4 +14,40 @@
         });
     }
 
+    // Testimonials carousel (uses the Owl Carousel library)
+    $(".trabajos-carousel").owlCarousel({
+        autoplay: true,
+        dots: true,
+        loop: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            768: {
+                items: 2
+            },
+            900: {
+                items: 3
+            }
+        }
+    });
+
 })(jQuery);
+
+function mostrarPaginas() {
+    document.getElementById("trabajos2").classList.add('noMostrar');
+    document.getElementById("trabajos3").classList.add('noMostrar');
+    document.getElementById("trabajos1").classList.remove('noMostrar');
+}
+
+function mostrarApps() {
+    document.getElementById("trabajos1").classList.add('noMostrar');
+    document.getElementById("trabajos3").classList.add('noMostrar');
+    document.getElementById("trabajos2").classList.remove('noMostrar');
+}
+
+function mostrarOtros() {
+    document.getElementById("trabajos2").classList.add('noMostrar');
+    document.getElementById("trabajos1").classList.add('noMostrar');
+    document.getElementById("trabajos3").classList.remove('noMostrar');
+}
