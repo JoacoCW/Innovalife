cbadbeaaed9bfe1e7957f989de3dc96e86a08d0c
JoaquinWeiss <joaco.w93@gmail.com>
2021-07-28 20:05:08 -0300

    worked on a carrousel for tecnologias section

--- assets/js/script.js
+++ assets/js/script.js
@@ -32,6 +32,24 @@
         }
     });
 
+    // Testimonials carousel (uses the Owl Carousel library)
+    $(".tecnologias-carousel").owlCarousel({
+        autoplay: true,
+        dots: true,
+        loop: true,
+        responsive: {
+            0: {
+                items: 2
+            },
+            768: {
+                items: 4
+            },
+            900: {
+                items: 6
+            }
+        }
+    });
+
 })(jQuery);
 
 function mostrarPaginas() {
